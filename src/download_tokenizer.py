985ba2a7c67d36196691c38e3dd001d597ef4656
Shawn Huang <seanzy0716@gmail.com>
2024-11-14 11:16:45 -0700

    updated tokenizer path

--- src/download_tokenizer.py
+++ src/download_tokenizer.py
@@ -3,6 +3,8 @@ from transformers import AutoTokenizer
 
 def download_tokenizer(model_path = "meta-llama/Llama-2-7b-chat-hf", token = ""):
     print("instantiating pretrained tokenizer")
-    AutoTokenizer.from_pretrained(model_path)
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    tokenizer.save_pretrained("./local_tokenizer")
+
 
 download_tokenizer()
